1c8cd8f75e79c1aef5a84466a92be1edd20524da
Alfonso Alba García <alfonso@ceronet.es>
2019-07-16 18:05:12 +0200

    Add seeds for TipoCultivo and TipoSuelo.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+TipoCultivo.create!(nombre: 'Olivos')
+TipoSuelo.create!(nombre: 'Arcilloso')
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
